812af8d56fc364fe7137a9175c33e0694f7fd79b
rafael <rafael@cotelha.(none)>
2010-05-05 20:05:55 -0300

    adicionado tipo de usuario na tabela de users e adicionado no insert o attributo

--- db/migrate/20100505225152_insert_user.rb
+++ db/migrate/20100505225152_insert_user.rb
@@ -1,6 +1,6 @@
 class InsertUser < ActiveRecord::Migration
   def self.up
-    Users.create :login=>"rafae", :name=>"rafae", :email=>"rafae.cote@gmail.com", :crypted_password=>"636c50b97f6cf278a0779f0cf48f2fd076534463", :salt=>"8a1284fc603d8cf14b51465aa408e37e9a317b49"
+    Users.create :login=>"rafael", :tp_usuario=>"A", :name=>"rafae", :email=>"rafae.cote@gmail.com", :crypted_password=>"636c50b97f6cf278a0779f0cf48f2fd076534463", :salt=>"8a1284fc603d8cf14b51465aa408e37e9a317b49"
   end
 
   def self.down
