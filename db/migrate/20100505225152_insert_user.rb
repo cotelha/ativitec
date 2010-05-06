2cd8a540819ed9a1db96d6d7a05ba38ff95145fe
rafael <rafael@cotelha.(none)>
2010-05-05 21:44:24 -0300

    adicionado traducoes, editar/excluir usuario

--- db/migrate/20100505225152_insert_user.rb
+++ db/migrate/20100505225152_insert_user.rb
@@ -1,9 +1,10 @@
 class InsertUser < ActiveRecord::Migration
   def self.up
-    Users.create :login=>"rafael", :tp_usuario=>"A", :name=>"rafae", :email=>"rafae.cote@gmail.com", :crypted_password=>"636c50b97f6cf278a0779f0cf48f2fd076534463", :salt=>"8a1284fc603d8cf14b51465aa408e37e9a317b49"
+    #User.create(:id=>1, :login=>'rafael', :tp_user=>'A', :name=>'rafael', :email=>'rafae.cote@gmail.com', :crypted_password=>'636c50b97f6cf278a0779f0cf48f2fd076534463', :salt=>'8a1284fc603d8cf14b51465aa408e37e9a317b49', :created_at=>nil, :updated_at=>nil, :remember_token_expires_at=>nil)
+    execute("insert into users(login,tp_user,name,email,crypted_password,salt) values('rafael','A','rafael','rafael.cote@gmail.com','636c50b97f6cf278a0779f0cf48f2fd076534463','8a1284fc603d8cf14b51465aa408e37e9a317b49');")
   end
 
   def self.down
-    Users.delete_all("login='rafael'")
+    User.delete_all("login='rafael'")
   end
 end
