fd475fa0ed0eace6f7a6492ac090550b24cddc6b
rafael <rafael@cotelha.(none)>
2010-05-06 21:25:14 -0300

    adicionado cadastro de projetos, adicionado partial do menu para todas as views

---
+++ app/models/projeto.rb
@@ -0,0 +1,3 @@
+class Projeto < ActiveRecord::Base
+  validates_presence_of :nm_projeto
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :projetos
+
   map.resources :clientes
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
