a6e91f8eaa8705dbfae5b83e00a1c09274287a07
rafael <rafael@cotelha.(none)>
2010-05-10 21:30:04 -0300

    adicionado tela inicial do sistema

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :motivo_atividades
-
   map.resources :atividades
-
   map.resources :projetos
-
   map.resources :clientes
+  map.resources :defaults, :only=>:index
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
