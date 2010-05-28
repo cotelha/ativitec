774b1685bbe26f88f77db75d17d062dc24ba1ce9
rafael <rafael@cotelha.(none)>
2010-05-27 22:51:14 -0300

    Adicionado botao de iniciar na inclusao de OS, alterado show da OS

--- app/controllers/orden_servicos_controller.rb
+++ app/controllers/orden_servicos_controller.rb
@@ -42,8 +42,7 @@ class OrdenServicosController < ApplicationController
   # POST /orden_servicos
   # POST /orden_servicos.xml
   def create    
-    params[:orden_servico][:ind_status] = "A"
-    params[:orden_servico][:atividade_id] = params[:cod_atividade]
+    params[:orden_servico][:ind_status] = "A"    
     params[:orden_servico][:dt_inicio] = Time.now.strftime("%d-%m-%Y %H:%M:%S").to_s
     @orden_servico = OrdenServico.new(params[:orden_servico])
 
