955654e69d80b7c83f34165981d184d04cfa0242
ftec <ftec@ftec-desktop.(none)>
2010-06-15 17:19:13 -0300

    adicionado zebrado

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,3 +22,13 @@ function abre_div(nome_div,check) {
       Effect.toggle(nome_div, 'blind', {duration:0.25});
     }
 }
+
+function tableZebra(tableID, classTR){ //Declaro a função
+	var tr = document.getElementById(tableID).getElementsByTagName("tr"); //Pego todas as "tr" dentro da tabela com "id" que recebemos
+	var trLength = tr.length; //Pega a quantidade/tamanho de "tr" existentes
+	for(var i = 0; i < trLength; i++){ //Percorro todos os "tr" no loop
+		if(i % 2 == 0){ //Conta matemática apenas para definir quem vai receber o "class"
+			tr[i].className = classTR; //Atenção aqui: para definir "class" no elemento, não se usa "element.class", e sim "element.className". Isso por que a palavra "class" é reservada
+		}; //Fecha instrução "if"
+	}; //Fecha loop "for"
+}; //Fecha bloco de função
